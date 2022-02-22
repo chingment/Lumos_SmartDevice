cc97760ad6bd6ae1817c47e91613dcd6ebce817e
qqw <qqw@uplink.net.cn>
2022-02-22 17:02:14 +0800

    s

--- lumos_caterbao/locals_biz/src/main/java/com/caterbao/lumos/locals/biz/service/BookerService.java
+++ lumos_caterbao/locals_biz/src/main/java/com/caterbao/lumos/locals/biz/service/BookerService.java
@@ -1,8 +1,11 @@
 package com.caterbao.lumos.locals.biz.service;
 
 
+import com.caterbao.lumos.locals.biz.model.BookerBorrowBook;
 import com.caterbao.lumos.locals.biz.model.BookerCalculateOverdueFineResult;
+import com.caterbao.lumos.locals.dal.pojo.BookBorrowReturnFlowData;
 
 public interface BookerService {
     BookerCalculateOverdueFineResult CalculateOverdueFine(String clientUserId);
+    BookerBorrowBook CalculateOverdueFine(int num, BookBorrowReturnFlowData bookBorrowReturnFlowData);
 }
--- lumos_caterbao/locals_biz/src/main/java/com/caterbao/lumos/locals/biz/service/impl/BookerServiceImpl.java
+++ lumos_caterbao/locals_biz/src/main/java/com/caterbao/lumos/locals/biz/service/impl/BookerServiceImpl.java
@@ -38,42 +38,13 @@ public class BookerServiceImpl implements BookerService {
         float sumSverdueFine=0;
         for (int i = 0; i < d_BookBorrowReturnFlowDatas.size(); i++) {
 
+
+
             BookBorrowReturnFlowData d_BookBorrowReturnFlowData = d_BookBorrowReturnFlowDatas.get(i);
 
-            BookerBorrowBook bookerBorrowBook = new BookerBorrowBook();
-            bookerBorrowBook.setSkuId(d_BookBorrowReturnFlowData.getSkuId());
-            bookerBorrowBook.setRfId(d_BookBorrowReturnFlowData.getSkuRfId());
-            bookerBorrowBook.setName(d_BookBorrowReturnFlowData.getSkuName());
-            bookerBorrowBook.setCumCode(d_BookBorrowReturnFlowData.getSkuCumCode());
-            bookerBorrowBook.setImgUrl(d_BookBorrowReturnFlowData.getSkuImgUrl());
-            bookerBorrowBook.setBorrowTime(CommonUtil.toDateTime(d_BookBorrowReturnFlowData.getBorrowTime()));
+            BookerBorrowBook bookerBorrowBook = CalculateOverdueFine(i,d_BookBorrowReturnFlowData);
 
-            long l = CommonUtil.getDateTimeNow().getTime() - d_BookBorrowReturnFlowData.getBorrowTime().getTime();
-            long diffDay = l / (24 * 60 * 60 * 1000);
-
-            float overdueFine = 0;
-
-            FieldModel status=new FieldModel();
-            if (diffDay <= 3) {
-                overdueFine = 0;
-                status=new FieldModel(1,"借阅中");
-            } else if (diffDay > 3 && diffDay <= 30) {
-                status=new FieldModel(2,"逾期借阅");
-                if(i<=2) {
-                    overdueFine = (diffDay - 3) * 0.5f;
-                }
-                else {
-                    overdueFine = (diffDay - 3) * 1f;
-                }
-            } else if (diffDay > 30) {
-                status=new FieldModel(3,"逾期借阅");
-                overdueFine =40f;
-            }
-
-            bookerBorrowBook.setStatus(status);
-            bookerBorrowBook.setOverdueFine(overdueFine);
-
-            sumSverdueFine += overdueFine;
+            sumSverdueFine += bookerBorrowBook.getOverdueFine();
 
             bookerBorrowBooks.add(bookerBorrowBook);
         }
@@ -81,5 +52,44 @@ public class BookerServiceImpl implements BookerService {
         result.setBorrowBooks(bookerBorrowBooks);
         result.setOverdueFine(sumSverdueFine);
         return result;
+    }
+
+    @Override
+    public BookerBorrowBook CalculateOverdueFine(int num,BookBorrowReturnFlowData bookBorrowReturnFlowData) {
+        BookerBorrowBook bookerBorrowBook = new BookerBorrowBook();
+
+        bookerBorrowBook.setSkuId(bookBorrowReturnFlowData.getSkuId());
+        bookerBorrowBook.setRfId(bookBorrowReturnFlowData.getSkuRfId());
+        bookerBorrowBook.setName(bookBorrowReturnFlowData.getSkuName());
+        bookerBorrowBook.setCumCode(bookBorrowReturnFlowData.getSkuCumCode());
+        bookerBorrowBook.setImgUrl(bookBorrowReturnFlowData.getSkuImgUrl());
+        bookerBorrowBook.setBorrowTime(CommonUtil.toDateTime(bookBorrowReturnFlowData.getBorrowTime()));
+
+        long l = CommonUtil.getDateTimeNow().getTime() - bookBorrowReturnFlowData.getBorrowTime().getTime();
+        long diffDay = l / (24 * 60 * 60 * 1000);
+
+        float overdueFine = 0;
+
+        FieldModel status = new FieldModel();
+        if (diffDay <= 3) {
+            overdueFine = 0;
+            status = new FieldModel(1, "借阅中");
+        } else if (diffDay > 3 && diffDay <= 30) {
+            status = new FieldModel(2, "逾期借阅");
+            if (num <= 2) {
+                overdueFine = (diffDay - 3) * 0.5f;
+            } else {
+                overdueFine = (diffDay - 3) * 1f;
+            }
+        } else if (diffDay > 30) {
+            status = new FieldModel(3, "逾期借阅");
+            overdueFine = 40f;
+        }
+
+        bookerBorrowBook.setStatus(status);
+        bookerBorrowBook.setOverdueFine(overdueFine);
+
+
+        return bookerBorrowBook;
     }
 }
